07816f5fd636918479e0869a9753ba277657e329
Antonio Andres Andreu Barreiro <aaandreu@ustspain.es>
2024-09-02 15:04:29 +0200

    feat: Se implementan consultas personalizadas

--- src/main/java/com/antonio/curso/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/antonio/curso/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -1,6 +1,7 @@
 package com.antonio.curso.springboot.jpa.springboot_jpa;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -11,11 +12,11 @@ import com.antonio.curso.springboot.jpa.springboot_jpa.entities.Person;
 import com.antonio.curso.springboot.jpa.springboot_jpa.repositories.PersonRepository;
 
 @SpringBootApplication
-public class SpringbootJpaApplication implements CommandLineRunner{
+public class SpringbootJpaApplication implements CommandLineRunner {
 
 	@Autowired
 	private PersonRepository repository;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootJpaApplication.class, args);
 	}
@@ -23,9 +24,40 @@ public class SpringbootJpaApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 
-		List<Person> persons = (List<Person>) repository.findAll();
+		findOne();
+	}
+
+	public void findOne() {
+
+		/*
+		 * Person person = null;
+		 * Optional<Person> optionalPerson = repository.findById(8L);
+		 * * if(!optionalPerson.isEmpty()) {
+		 * if(optionalPerson.isPresent()) {
+		 * person = optionalPerson.get();
+		 * }
+		 * System.out.println(person);
+		 */
+
+		 // repository.findById(1L).ifPresent(System.out::println);
+
+		 repository.findByNameContaining("hn").ifPresent(System.out::println);
+	}
+
+	public void list() {
+		// List<Person> persons = (List<Person>) repository.findAll();
+		// List<Person> persons = (List<Person>)
+		// repository.findByProgrammingLanguage("javaScript");
+
+		// List<Person> persons = (List<Person>)
+		// repository.buscarByProgrammingLanguage("Python", "Pepe");
+		List<Person> persons = (List<Person>) repository.findByProgrammingLanguageAndName("java", "Andres");
 
 		persons.stream().forEach(person -> System.out.println(person));
+
+		List<Object[]> personsValues = repository.obtenerPersonDataByProgrammingLanguage("java");
+		personsValues.stream().forEach(person -> System.out.println(person[0] + " es experto en " + person[1]));
+
 	}
 
 }
